07a5e2398f1ef830e55f12b1b8dc8231ceede60f
Lwelly Bah <lwellyebah@gmail.com>
2025-05-31 00:58:39 +0000

    just testing something

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../services/usersService/user.service';
+import { User } from '../../interfaces/user';
+import { PlaintesService } from '../../services/plaintesService/plaintes.service';
+import { Plaintes } from '../../interfaces/plaintes';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  private usersService = inject(UserService);
+  users : User[] = [];
+  usersNumber : number = 0;
+  blockedUsersNumber : number = 0;
+  chefsNumber : number = 0;
 
+  private plaintesService = inject(PlaintesService);
+  plaintes : Plaintes[] = [];
+  plaintesNumber : number = 0;
+  plaintesNonInspectNumber : number = 0;
+  invalidPlaintesNumber : number = 0;
+
+  loadData () {
+      forkJoin ({
+        plaintes : this.plaintesService.getPlaintes(),
+        users : this.usersService.getUsers()
+      }).subscribe( ({ plaintes, users }) => {
+        this.usersNumber = users.filter(user => user.role == 'CITOYEN').length;
+        this.blockedUsersNumber = users.filter(user => user.blocquee == 'blocquee').length;
+        this.chefsNumber = users.filter(user => user.role == 'CHEF').length;
+
+        this.plaintesNumber = plaintes.filter(plainte => plainte.examiner == 'examinee' && plainte.valid == 'valid').length;
+        this.invalidPlaintesNumber = plaintes.filter(plainte => plainte.valid !== 'valid').length;
+        this.plaintesNonInspectNumber = plaintes.filter(plainte => plainte.examiner !== 'examinee').length;
+      })
+  }
+
+  ngOnInit () {
+    this.loadData()
+  }
 }
